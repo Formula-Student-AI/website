b2613292bd45fb4bb6f388fa5c81c300aa4fe9b1
Sank6 <25014241+Sank6@users.noreply.github.com>
2023-10-17 16:44:43 +0100

    styled

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,10 @@ import fs from 'fs'
 import matter from 'gray-matter'
 import Link from 'next/link'
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home({ events }) {
+export default function Home({ eventsBeforeNow, eventsAfterNow }) {
   return (<div className={styles['container']}>
     <Head>
       <title>Formula Student AI</title>
@@ -12,12 +13,27 @@ export default function Home({ events }) {
     <h1 className={styles.header}>Welcome to Formula Student AI</h1>
     <p className={styles.subtitle}>Coming Up</p>
     <div className={styles.cardsContainer}>
-      {events.map(event => (
-        <div key={event.slug} className={styles.card}>
-          <Link href={`/event/${event.slug}`}>
-            <span>{event.title} - {event.date}</span>
-          </Link>
-        </div>
+      {eventsAfterNow.length ? eventsAfterNow.map(event => (
+        <Link href={`/event/${event.slug}`} key={event.slug}>
+          <div className={styles.card}>
+            <Image src={event.image} alt={event.title} width={400} height={200} />
+            <span className={styles.title}>{event.title}</span>
+            <span className={styles.date}>{event.date}</span>
+          </div>
+        </Link>
+      )) : <p className={styles.noevents}>No upcoming events</p>}
+    </div>
+    <hr className={styles.hr} />
+    <p className={styles.subtitle}>Past Events</p>
+    <div className={styles.cardsContainer}>
+      {eventsBeforeNow.map(event => (
+        <Link href={`/event/${event.slug}`} key={event.slug}>
+          <div className={styles.card}>
+            <Image src={event.image} alt={event.title} width={400} height={200} />
+            <span className={styles.title}>{event.title}</span>
+            <span className={styles.date}>{(new Date(event.date)).toLocaleString()}</span>
+          </div>
+        </Link>
       ))}
     </div>
   </div>)
@@ -28,19 +44,31 @@ export async function getStaticProps() {
   const filesInEvents = fs.readdirSync('./content/events')
 
   // Get the front matter and slug (the filename without .md) of all files
-  const events = filesInEvents.map(filename => {
-    const file = fs.readFileSync(`./content/events/${filename}`, 'utf8')
-		const matterData = matter(file)
-		
-    return {
-			...matterData.data, // matterData.data contains front matter
-			slug: filename.slice(0, filename.indexOf('.'))
-		}
-	})
+  const events = (filesInEvents
+    .map(filename => {
+      const file = fs.readFileSync(`./content/events/${filename}`, 'utf8')
+      const matterData = matter(file)
+
+      return {
+        ...matterData.data, // matterData.data contains front matter
+        slug: filename.slice(0, filename.indexOf('.'))
+      }
+    }) as { [key: string]: any }[])
+    .map(event => {
+      return {
+        ...event,
+        date: (new Date(event.date.replace(/([0-9]{2})\/([0-9]{2})\/([0-9]{4}) ([0-9]{2}):([0-9]{2})/gm, "$3-$2-$1T$4:$5:00"))).getTime()
+      }
+    })
+
+  const eventsBeforeNow = events.filter(event => event.date < Date.now()).sort((a, b) => b.date - a.date)
+  const eventsAfterNow = events.filter(event => event.date > Date.now()).sort((a, b) => a.date - b.date)
+
 
   return {
     props: {
-      events
+      eventsBeforeNow,
+      eventsAfterNow
     }
   }
 
