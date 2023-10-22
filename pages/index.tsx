f9573cd68258095bc9e0ec93f1c7fb43bf7ceb90
Sank6 <25014241+Sank6@users.noreply.github.com>
2023-10-22 14:00:01 +0100

    posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,37 +4,53 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import ReactMarkdown from 'react-markdown'
 
-export default function Home({ eventsBeforeNow, eventsAfterNow }) {
+export default function Home({ eventsBeforeNow, eventsAfterNow, posts }) {
   return (<div className={styles['container']}>
     <Head>
       <title>Formula Student AI</title>
     </Head>
     <h1 className={styles.header}>Welcome to Formula Student AI</h1>
-    <p className={styles.subtitle}>Coming Up</p>
-    <div className={styles.cardsContainer}>
-      {eventsAfterNow.length ? eventsAfterNow.map(event => (
-        <Link href={`/event/${event.slug}`} key={event.slug}>
-          <div className={styles.card}>
-            <Image src={event.image} alt={event.title} width={400} height={200} />
-            <span className={styles.title}>{event.title}</span>
-            <span className={styles.date}>{event.date}</span>
-          </div>
-        </Link>
-      )) : <p className={styles.noevents}>No upcoming events</p>}
+    <div className={styles.events}>
+      <p className={styles.subtitle}>Coming Up</p>
+      <div className={styles.cardsContainer}>
+        {eventsAfterNow.length ? eventsAfterNow.map(event => (
+          <Link href={`/event/${event.slug}`} key={event.slug}>
+            <div className={styles.card}>
+              <Image src={event.image} alt={event.title} width={400} height={200} />
+              <span className={styles.title}>{event.title}</span>
+              <span className={styles.date}>{event.date}</span>
+            </div>
+          </Link>
+        )) : <p className={styles.noevents}>No upcoming events</p>}
+      </div>
+      <hr className={styles.hr} />
+      <p className={styles.subtitle}>Past Events</p>
+      <div className={styles.cardsContainer}>
+        {eventsBeforeNow.map(event => (
+          <Link href={`/event/${event.slug}`} key={event.slug}>
+            <div className={styles.card}>
+              <Image src={event.image} alt={event.title} width={400} height={200} />
+              <span className={styles.title}>{event.title}</span>
+              <span className={styles.date}>{(new Date(event.date)).toLocaleString()}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-    <hr className={styles.hr} />
-    <p className={styles.subtitle}>Past Events</p>
-    <div className={styles.cardsContainer}>
-      {eventsBeforeNow.map(event => (
-        <Link href={`/event/${event.slug}`} key={event.slug}>
-          <div className={styles.card}>
-            <Image src={event.image} alt={event.title} width={400} height={200} />
-            <span className={styles.title}>{event.title}</span>
-            <span className={styles.date}>{(new Date(event.date)).toLocaleString()}</span>
+    <div className={styles.posts}>
+      <p className={styles.subtitle}>Latest Posts</p>
+      <div className={styles.cardsContainer}>
+        {posts.slice(0, 3).map(post => (
+          <div className={styles.post} key={post.slug}>
+            <h1>{post.title}</h1>
+            <div className={styles.date}>{(new Date(post.date)).toLocaleString()}</div>
+            {(post.link) ? <Link href={post.link}><a className={styles.link}>Link</a></Link> : null}
+            <ReactMarkdown>{post.content}</ReactMarkdown>
           </div>
-        </Link>
-      ))}
+        ))}
+      </div>
     </div>
   </div>)
 }
@@ -59,16 +75,38 @@ export async function getStaticProps() {
         ...event,
         date: (new Date(event.date.replace(/([0-9]{2})\/([0-9]{2})\/([0-9]{4}) ([0-9]{2}):([0-9]{2})/gm, "$3-$2-$1T$4:$5:00"))).getTime()
       }
+    });
+
+  const eventsBeforeNow = events.filter(event => event.date < Date.now()).sort((a, b) => b.date - a.date);
+  const eventsAfterNow = events.filter(event => event.date > Date.now()).sort((a, b) => a.date - b.date);
+
+  const filesInPosts = fs.readdirSync('./content/posts')
+  const posts = (filesInPosts
+    .map(filename => {
+      const file = fs.readFileSync(`./content/posts/${filename}`, 'utf8')
+      const matterData = matter(file)
+
+      return {
+        ...matterData.data, // matterData.data contains front matter
+        content: matterData.content,
+        slug: filename.slice(0, filename.indexOf('.'))
+      }
+    }) as { [key: string]: any }[])
+    .map(post => {
+      return {
+        ...post,
+        date: (new Date(post.date.replace(/([0-9]{2})\/([0-9]{2})\/([0-9]{4}) ([0-9]{2}):([0-9]{2})/gm, "$3-$2-$1T$4:$5:00"))).getTime()
+      }
     })
+    .sort((a, b) => b.date - a.date);
 
-  const eventsBeforeNow = events.filter(event => event.date < Date.now()).sort((a, b) => b.date - a.date)
-  const eventsAfterNow = events.filter(event => event.date > Date.now()).sort((a, b) => a.date - b.date)
-
+  console.log(posts)
 
   return {
     props: {
       eventsBeforeNow,
-      eventsAfterNow
+      eventsAfterNow,
+      posts
     }
   }
 
---
+++ pages/post/[slug].tsx
@@ -0,0 +1,48 @@
+import fs from 'fs'
+import ReactMarkdown from 'react-markdown'
+import matter from 'gray-matter'
+import Head from 'next/head'
+import styles from '../../styles/Event.module.css'
+
+export default function Event({ frontmatter, markdown }) {
+	return (
+		<div className={styles['container']}>
+			<Head>
+				<title>Formula Student AI Post | {frontmatter.title}</title>
+			</Head>
+			<h1 className={styles['title']}>{frontmatter.title}</h1>
+			<div>{frontmatter.date}</div>
+            <a href={frontmatter.link} target="_blank" rel="noopener noreferrer">Link</a>
+			<hr />
+			<div className={styles['wrapper']}>
+				<ReactMarkdown>
+					{markdown}
+				</ReactMarkdown>
+			</div>
+		</div>
+	)
+}
+
+export async function getStaticProps({ params: { slug } }) {
+	const fileContent = matter(fs.readFileSync(`./content/posts/${slug}.md`, 'utf8'))
+	let frontmatter = fileContent.data
+	const markdown = fileContent.content
+
+	return {
+		props: { frontmatter, markdown }
+	}
+}
+
+export async function getStaticPaths() {
+	const filesInProjects = fs.readdirSync('./content/posts')
+
+	const paths = filesInProjects.map(file => {
+		const filename = file.slice(0, file.indexOf('.'))
+		return { params: { slug: filename } }
+	})
+
+	return {
+		paths,
+		fallback: false // This shows a 404 page if the page is not found
+	}
+}
