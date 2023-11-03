aed8cc09f4fce54ab87c9d6cf89a0545c688b971
Sank6 <25014241+Sank6@users.noreply.github.com>
2023-11-03 16:13:51 +0000

    fix date

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home({ eventsBeforeNow, eventsAfterNow, posts }) {
             <div className={styles.card}>
               <Image src={event.image} alt={event.title} width={400} height={200} />
               <span className={styles.title}>{event.title}</span>
-              <span className={styles.date}>{event.date}</span>
+              <span className={styles.date}>{(new Date(event.date)).toLocaleString()}</span>
             </div>
           </Link>
         )) : <p className={styles.noevents}>No upcoming events</p>}
