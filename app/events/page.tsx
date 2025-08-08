4d9850a9b71f2b1d48240efd46331902f6dff59b
TomLam <136619817+T0mLam@users.noreply.github.com>
2025-08-09 00:48:41 +0800

    Add metadata for Events page,

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -2,6 +2,19 @@ import Container from "../components/common/container";
 import { Intro } from "../components/events/intro";
 import { MajorEvent } from "../components/events/major-event";
 import { getAllFutureEvents } from "@/lib/eventApi";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Events | Bristol Formula Student AI",
+  description:
+    "Check out the latest events from the Bristol Formula Student AI team.",
+  openGraph: {
+    title: "Events | Bristol Formula Student AI",
+    description:
+      "Check out the latest events from the Bristol Formula Student AI team.",
+    type: "website",
+  },
+};
 
 export default function Events() {
   const futureEvents = getAllFutureEvents();
